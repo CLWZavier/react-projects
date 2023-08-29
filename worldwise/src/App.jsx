b5fdcfc005b8c0b59ce553a1634576c5cb6a7a63
Zavier <zavierchan1999@gmail.com>
2023-08-29 16:43:34 +0800

    feat: add list of cities

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -5,16 +5,54 @@ import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
+import CityList from "./components/CityList";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const BASE_URL = "http://localhost:9000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        // try-catch block
+        setIsLoading(true);
+        const response = await fetch(`${BASE_URL}/cities`);
+        const data = await response.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          {/* index = Default child route */}
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- worldwise/src/components/Sidebar.jsx
+++ worldwise/src/components/Sidebar.jsx
@@ -2,12 +2,14 @@ import AppNav from "./AppNav";
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import CrFooter from "./CrFooter";
+import { Outlet } from "react-router-dom";
 
 function Sidebar() {
   return (
     <div className={styles.sidebar}>
-      <Logo />;
-      <AppNav />;<p>List of cities</p>
+      <Logo />
+      <AppNav />
+      <Outlet />
       <CrFooter />
     </div>
   );
