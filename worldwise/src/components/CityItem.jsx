0b88104226c1d8623417ac2b2cdee66239a6538e
Zavier <zavierchan1999@gmail.com>
2023-09-03 22:10:58 +0800

    feat: add URL parameters and query strings for id, coordinates of city

--- worldwise/src/components/CityItem.jsx
+++ worldwise/src/components/CityItem.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
 
 const formatDate = (date) =>
@@ -7,17 +9,20 @@ const formatDate = (date) =>
     year: "numeric",
   }).format(new Date(date));
 
-// eslint-disable-next-line react/prop-types, no-unused-vars
 function CityItem({ city }) {
-  // eslint-disable-next-line react/prop-types
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id, position } = city;
 
   return (
-    <li className={styles.cityItem}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>{formatDate(date)}</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link
+        className={styles.cityItem}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>{formatDate(date)}</time>
+        <button className={styles.deleteBtn}>&times;</button>
+      </Link>
     </li>
   );
 }
