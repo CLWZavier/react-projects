d0b209f86b82fe2a69b8b8302e05b77a8a0de0d2
Zavier <zavierchan1999@gmail.com>
2023-09-11 21:53:02 +0800

    refactor: add useContext to replace prop drilling

---
+++ worldwise/src/contexts/CitiesContext.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable react/prop-types */
+import { useContext } from "react";
+import { createContext, useState, useEffect } from "react";
+
+const BASE_URL = "http://localhost:9000";
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        // try-catch block
+        setIsLoading(true);
+        const response = await fetch(`${BASE_URL}/cities`);
+        const data = await response.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data...");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined) {
+    // Trying to access the content value in a place which is not a child component of this provider
+    throw new Error(`CitiesContext was ussed outside the CitiesProvider`);
+  }
+  return context;
+}
+
+export { CitiesProvider, useCities };
